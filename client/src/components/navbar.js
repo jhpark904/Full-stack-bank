18a9e62e9b31d72271b5467a484c99e62fed2b12
Jung Hwan Park <jhpark904@gmail.com>
2023-07-09 17:54:10 -0400

    hide deposit and withdraw when not logged in

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import { Tooltip } from "react-bootstrap";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../auth/firebase";
 
 const tooltip = (displayInfo) => {
   return <Tooltip>{displayInfo}</Tooltip>;
 };
 
 const NavBar = () => {
+  const [signedIn, setSignedIn] = useState(auth.currentUser != null);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setSignedIn(true);
+    } else {
+      setSignedIn(false);
+    }
+  });
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="#/">
-        BadBank
+        Best Bank
       </a>
       <button
         className="navbar-toggler"
@@ -47,39 +59,43 @@ const NavBar = () => {
               </a>
             </li>
           </OverlayTrigger>
-          <OverlayTrigger
-            placement="bottom"
-            overlay={tooltip("Navigate to deposit")}
-            triggers={["hover"]}
-          >
-            <li className="nav-item">
-              <a className="nav-link" href="#/deposit/">
-                Deposit
-              </a>
-            </li>
-          </OverlayTrigger>
-          <OverlayTrigger
-            placement="bottom"
-            overlay={tooltip("Navigate to withdraw")}
-            triggers={["hover"]}
-          >
-            <li className="nav-item">
-              <a className="nav-link" href="#/withdraw/">
-                Withdraw
-              </a>
-            </li>
-          </OverlayTrigger>
-          <OverlayTrigger
-            placement="bottom"
-            overlay={tooltip("Navigate to view customer data")}
-            triggers={["hover"]}
-          >
-            <li className="nav-item">
-              <a className="nav-link" href="#/alldata/">
-                All Data
-              </a>
-            </li>
-          </OverlayTrigger>
+          {signedIn && (
+            <>
+              <OverlayTrigger
+                placement="bottom"
+                overlay={tooltip("Navigate to deposit")}
+                triggers={["hover"]}
+              >
+                <li className="nav-item">
+                  <a className="nav-link" href="#/deposit/">
+                    Deposit
+                  </a>
+                </li>
+              </OverlayTrigger>
+              <OverlayTrigger
+                placement="bottom"
+                overlay={tooltip("Navigate to withdraw")}
+                triggers={["hover"]}
+              >
+                <li className="nav-item">
+                  <a className="nav-link" href="#/withdraw/">
+                    Withdraw
+                  </a>
+                </li>
+              </OverlayTrigger>
+              <OverlayTrigger
+                placement="bottom"
+                overlay={tooltip("Navigate to view customer data")}
+                triggers={["hover"]}
+              >
+                <li className="nav-item">
+                  <a className="nav-link" href="#/alldata/">
+                    All Data
+                  </a>
+                </li>
+              </OverlayTrigger>
+            </>
+          )}
         </ul>
       </div>
     </nav>
