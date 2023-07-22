597f8c083ddff6bbb771b99fd496f030825aa6fe
Jung Hwan Park <jhpark904@gmail.com>
2023-07-21 21:29:30 -0400

    change api Url

--- client/src/components/context.js
+++ client/src/components/context.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route, Link, HashRouter } from "react-router-dom";
 
-const apiUrl = `http://localhost:8080`;
+const apiUrl = "https://best-bank-c40ef5126795.herokuapp.com";
 
 const classes = (bgcolor, txtcolor) => {
   const bg = bgcolor ? " bg-" + bgcolor : " ";
